014dc2806f5369c9cc7130687d38925e1203d95a
Komal2006 <komaldeepkr99@gmail.com>
2019-10-31 13:16:42 -0400

    [komal] login viewController

---
+++ C0766343_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  C0766343_MidTerm_MAD3115F2019
+//
+//  Created by MacStudent on 2019-10-31.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
+    @IBOutlet var RememberMeBtn: UIView!
+    
+    @IBAction func btnLogin(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- C0766343_MidTerm_MAD3115F2019/ViewController.swift
+++ C0766343_MidTerm_MAD3115F2019/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func btnLogin(_ sender: UIBarButtonItem) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
