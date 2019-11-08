43a28708326af9c4813053b9bd6edec57b5ffe61
Komal2006 <komaldeepkr99@gmail.com>
2019-11-08 13:37:12 -0500

    [komal] internet class

--- C0766343_MidTerm_MAD3115F2019/Bills/Internet.swift
+++ C0766343_MidTerm_MAD3115F2019/Bills/Internet.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+class Internet: Bill{
+    
+    var providerName: String
+    var internetUsed: Int
+    init(billId: Int, billDate: String, billType: TypesOfBill, billAmount: Float, providerName: String,internetUsed: Int )
+    {
+        
+        
+        self.providerName = providerName
+        self.internetUsed = internetUsed
+        super.init(billId: billId, billDate: billDate, billType: billType, billAmount: billAmount)
+    }
+    
+    override func display()
+    {
+        super.display()
+        print("Provider Name: \(providerName)")
+        print("Internet Used: \(internetUsed.insertGB())")
+    }
+    
+}
