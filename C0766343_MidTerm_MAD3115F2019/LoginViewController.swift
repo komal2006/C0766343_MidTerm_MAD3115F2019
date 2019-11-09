f42ea0869b66f26c90ee564a3dbb301d9f51d99c
Komal2006 <komaldeepkr99@gmail.com>
2019-11-08 19:34:04 -0500

    [komal] loginVC

--- C0766343_MidTerm_MAD3115F2019/LoginViewController.swift
+++ C0766343_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -10,98 +10,90 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var txtEmail: UITextField!
-    
-    @IBOutlet weak var txtPassword: UITextField!
-    
-    @IBOutlet weak var btnLogin: UIBarButtonItem!
-    
-    @IBOutlet weak var btnSwitch: UISwitch!
-    var uDefault:UserDefaults!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-        let getdata = Singleton.getInstance()
-        getdata.createCust()
-        
-        uDefault  = UserDefaults.standard
-        if let email = uDefault.value(forKey: "Email"){
-            txtEmail.text = email as? String
-        }
-            
-       
-        
-        if let Password = uDefault.value(forKey: "Password"){
-            txtPassword.text = Password as? String
-        }
-        
-    }
+    @IBOutlet weak var txtUser: UITextField!
+       @IBOutlet weak var txtPassword: UITextField!
+     
+       @IBOutlet weak var btnLogin: UIButton!
+     var uDefault : UserDefaults!
+    @IBOutlet weak var btnRememberMe: UISwitch!
     
    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+        
+        
+        
+         let getdata = Singleton.getInstance()
+               getdata.createCustomer()
+
+        uDefault  = UserDefaults.standard
+        if let userName = uDefault.value(forKey: "userName"){
+            txtUser.text = userName as? String
+        }
+        
+        if let userPassword = uDefault.value(forKey: "userPassword"){
+            txtPassword.text = userPassword as? String
+        }
+       }
+   
+    
     @IBAction func btnLogin(_ sender: UIButton) {
         
-               if readInformationPlist()
-               {
-            if self.btnSwitch.isOn{
-                self.uDefault.set(txtEmail.text, forKey: "Email")
-                self.uDefault.set(txtPassword.text, forKey: "Password")
-            }else
-            {
-                self.uDefault.removeObject(forKey: "Email")
-                self.uDefault.removeObject(forKey: "Password")
+        if readInformationPlist()
+                {
+                    if self.btnRememberMe.isOn{
+                        self.uDefault.set(txtUser.text, forKey: "userName")
+                        self.uDefault.set(txtPassword.text, forKey: "userPassword")
+                    }else{
+                        self.uDefault.removeObject(forKey: "userName")
+                        self.uDefault.removeObject(forKey: "userPassword")
+                    }
+                    
+                    
+                    let sb=UIStoryboard(name: "Main", bundle: nil)
+                           let customerListVC=sb.instantiateViewController(withIdentifier: "customerListVC") as! CustomerListViewController
+                           navigationController?.pushViewController(customerListVC, animated: true)
+
+//                let uname=txtUser.text!
+//                print("Hello \(uname)")
             }
-            
-            
-        let sb = UIStoryboard(name: "Main", bundle: nil)
-       let CustomerVC = sb.instantiateViewController(withIdentifier: "customerListVC") as! CustomerListTableViewController
+                else{
+                    let alert = UIAlertController(title: "Wrong Username Or Password", message: "change username or password", preferredStyle: .alert)
 
-
-       navigationController?.pushViewController(CustomerVC, animated: true)
-                
-        }else
-        {
-            let alert = UIAlertController(title: "Invalid Email Or Password", message: "Enter valid Email or Password", preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
-            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-           
-            self.present(alert, animated: true)
-            print("Incorrect Email or Password")
-        }
+                    alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: {action
+                        in
+                        print("Cancle clicked")
+                    }))
+                    self.present(alert, animated: true)
+                    print("invalid username or password")
+                }
         
     }
+   
+    
+    
+    func readInformationPlist() -> Bool{
+       if let bundlePath = Bundle.main.path(forResource: "Users", ofType: "plist") {
+           let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+            let usersList = dictionary!["Users"] as! NSArray
         
-        
-        func readInformationPlist() -> Bool{
-            if let bundlePath = Bundle.main.path(forResource: "Customers", ofType: "plist") {
-                let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
-                let CustomersList = dictionary!["Customers"] as! NSArray
-                
-                for c in CustomersList
+            for u in usersList
+            {
+                 let user = u as! NSDictionary
+                let uname = user["username"]! as! String
+                let pwd = user["password"]! as! String
+                if uname==txtUser.text! && pwd==txtPassword.text!
                 {
-                    let customer = c as! NSDictionary
-                    let email = customer["Email"]! as! String
-                    let pwd = customer["Password"]! as! String
-                    if email==txtEmail.text! && pwd==txtPassword.text!
-                    {
-                        return true
-                    }
+                    return true
                 }
-                
-                
             }
+        
+       
+           }
             return false
-        }
-        
-        
-
-
-
+       }
+       
+    
 }
-
-
 
